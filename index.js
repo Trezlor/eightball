8a404eae67fa814d18fe50b1d27d7e5f0e926c39
SondreHal <sondrehal.kodehode@gmail.com>
2022-06-22 15:07:54 +0200

    hoi

--- index.js
+++ index.js
@@ -1,48 +1,31 @@
-function another() {
-	const randomNumber = Math.floor(Math.random() * 20 + 1);
-	const eightBall = document.getElementById("prediction");
+const options = [
+	"It is certain.",
+	"It is decidedly so.",
+	"Without a doubt.",
+	"Yes definitely.",
+	"You may rely on it.",
+	"As I see it, yes.",
+	"Most likely.",
+	"Outlook good.",
+	"Yes.",
+	"Signs point to yes.",
+	"Reply hazy, try again.",
+	"Ask again later.",
+	"Better not tell you now.",
+	"Cannot predict now.",
+	"Concentrate and ask again.",
+	"Don't count on it.",
+	"My reply is no.",
+	"My sources say no.",
+	"Outlook not so good.",
+	"Very doubtful.",
+];
 
-	if (randomNumber === 1) {
-		eightBall.textContent = "It is certain.";
-	} else if (randomNumber === 2) {
-		eightBall.textContent = "It is decidedly so.";
-	} else if (randomNumber === 3) {
-		eightBall.textContent = "Without a doubt.";
-	} else if (randomNumber === 4) {
-		eightBall.textContent = "Yes definitely.";
-	} else if (randomNumber === 5) {
-		eightBall.textContent = "You may rely on it.";
-	} else if (randomNumber === 6) {
-		eightBall.textContent = "As I see it, yes.";
-	} else if (randomNumber === 7) {
-		eightBall.textContent = "Most likely.";
-	} else if (randomNumber === 8) {
-		eightBall.textContent = "Outlook good.";
-	} else if (randomNumber === 9) {
-		eightBall.textContent = "Yes.";
-	} else if (randomNumber === 10) {
-		eightBall.textContent = "Signs point to yes.";
-	} else if (randomNumber === 11) {
-		eightBall.textContent = "Reply hazy, try again. ";
-	} else if (randomNumber === 12) {
-		eightBall.textContent = "Ask again later.";
-	} else if (randomNumber === 13) {
-		eightBall.textContent = "Better not tell you now.";
-	} else if (randomNumber === 14) {
-		eightBall.textContent = "Cannot predict now.";
-	} else if (randomNumber === 15) {
-		eightBall.textContent = "Concentrate and ask again.";
-	} else if (randomNumber === 16) {
-		eightBall.textContent = "Don't count on it.";
-	} else if (randomNumber === 17) {
-		eightBall.textContent = "My reply is no.";
-	} else if (randomNumber === 18) {
-		eightBall.textContent = "My sources say no.";
-	} else if (randomNumber === 19) {
-		eightBall.textContent = "Outlook not so good.";
-	} else if (randomNumber === 20) {
-		eightBall.textContent = "Very doubtful.";
-	}
+const prediction = document.getElementById("prediction");
+
+function another() {
+	const random = Math.floor(Math.random() * options.length);
+	prediction.textContent = options[random];
 }
 
 another();
